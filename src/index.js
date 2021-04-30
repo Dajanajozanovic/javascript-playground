6ded1bae5d5e6e87839357ffc150a72e86eefd58
Dajana Jozanovic <dajana.jozanovic@t-online.de>
2021-04-30 12:26:06 +0200

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,76 +1,62 @@
-const buttonHome = document.querySelector('.button-home')
-const buttonBookmarks = document.querySelector('.button-bookmarks')
-const buttonCreate = document.querySelector('.button-create')
-const buttonSettings = document.querySelector('.button-settings')
+const buttonHome = getElement('.button-home')
+const buttonBookmarks = getElement('.button-bookmarks')
+const buttonCreate = getElement('.button-create')
+const buttonSettings = getElement('.button-settings')
 
-const homePage = document.querySelector('.page-home')
-const bookmarksPage = document.querySelector('.page-bookmarks')
-const createPage = document.querySelector('.page-create')
-const settingsPage = document.querySelector('.page-settings')
+const homePage = getElement('.page-home')
+const bookmarksPage = getElement('.page-bookmarks')
+const createPage = getElement('.page-create')
+const settingsPage = getElement('.page-settings')
 
-const hideAllpages = () => {
+buttonHome.addEventListener('click', navigateToHome)
+buttonBookmarks.addEventListener('click', navigateToBookmarks)
+buttonCreate.addEventListener('click', navigateToCreate)
+buttonSettings.addEventListener('click', navigateToSettings)
+
+function navigateToHome() {
+  changePage(homePage)
+  selectButton(buttonHome)
+}
+
+function navigateToBookmarks() {
+  changePage(bookmarksPage)
+  selectButton(buttonBookmarks)
+}
+
+function navigateToCreate() {
+  changePage(createPage)
+  selectButton(buttonCreate)
+}
+
+function navigateToSettings() {
+  changePage(settingsPage)
+  selectButton(buttonSettings)
+}
+
+function changePage(page) {
+  hideAllPages()
+  page.classList.remove('hidden')
+}
+
+function selectButton(button) {
+  deactivateButtons()
+  button.classList.add('active')
+}
+
+function hideAllPages() {
   homePage.classList.add('hidden')
   bookmarksPage.classList.add('hidden')
   createPage.classList.add('hidden')
   settingsPage.classList.add('hidden')
 }
 
-const deactivateButtons = () => {
-  buttonHome.classList.remove('active')
-  buttonBookmarks.classList.remove('active')
-  buttonCreate.classList.remove('active')
-  buttonSettings.classList.remove('active')
-}
-
-buttonHome.addEventListener('click', () => {
-  hideAllpages()
-  homePage.classList.remove('hidden')
-})
-
-buttonHome.addEventListener('click', () => {
-  deactivateButtons()
-  buttonHome.classList.add('active')
-})
-
-buttonBookmarks.addEventListener('click', () => {
-  hideAllpages()
-  bookmarksPage.classList.remove('hidden')
-})
-
-buttonBookmarks.addEventListener('click', () => {
-  deactivateButtons()
-  buttonBookmarks.classList.add('active')
-})
-
-buttonCreate.addEventListener('click', () => {
-  hideAllpages()
-  createPage.classList.remove('hidden')
-})
-
-buttonCreate.addEventListener('click', () => {
-  deactivateButtons()
-  buttonCreate.classList.add('active')
-})
-
-buttonSettings.addEventListener('click', () => {
-  hideAllpages()
-  settingsPage.classList.remove('hidden')
-})
-
-buttonSettings.addEventListener('click', () => {
-  deactivateButtons()
-  buttonSettings.classList.add('active')
-})
-
-function hideAllpages() {
-  homePage.classList.add('hidden')
-  bookmarksPage.classList.add('hidden')
-  createPage.classList.add('hidden')
-  settingsPage.classList.add('hidden')
-}
 function deactivateButtons() {
   buttonHome.classList.remove('active')
   buttonBookmarks.classList.remove('active')
   buttonCreate.classList.remove('active')
   buttonSettings.classList.remove('active')
+}
+
+function getElement(selector) {
+  return document.querySelector(selector)
 }
